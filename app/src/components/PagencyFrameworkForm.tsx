7fb89def9bc6df161ef17dde61a4d0039c8b985b
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-04-26 21:51:13 +0200

    progress bar

--- app/src/components/PagencyFrameworkForm.tsx
+++ app/src/components/PagencyFrameworkForm.tsx
@@ -3,6 +3,7 @@ import frameworkItems from "@/data/pagencyFramework.json"
 import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { saveFormData, getFormData } from "@/utils/indexedDB"
+import Link from "next/link"
 
 interface Props {
   itemId: number
@@ -12,6 +13,8 @@ export default function PagencyFrameworkForm({ itemId }: Props) {
   const router = useRouter()
   const item = (frameworkItems as FrameworkItem[]).find((item) => item.id === String(itemId))
   const [formData, setFormData] = useState<Record<string, string>>({})
+
+  const bars = Array.from({ length: frameworkItems.length }, (_, i) => i + 1)
 
   useEffect(() => {
     const loadSavedData = async () => {
@@ -64,47 +67,67 @@ export default function PagencyFrameworkForm({ itemId }: Props) {
   }
 
   return (
-    <form
-      className="flex flex-col thin-border w-full max-w-3xl self-center p-5 px-3 sm:px-5 text-white gap-4"
-      onSubmit={handleSubmit}
-    >
-      <h2 className="text-xl font-bold">
-        {item.id}. {item.title}
-      </h2>
-      <p className="text-white text-sm">{item.description}</p>
-
-      {item.sections.map((section, index) => (
-        <div key={index} className="flex flex-col gap-2 thin-border p-5 px-3 sm:px-5">
-          <h3 className="text-green font-bold">{section.title}</h3>
-          {section.questions.map((question, qIndex) => (
-            <div key={qIndex} className="flex flex-col gap-1 py-2">
-              <label className="text-sm">{question}</label>
-              <textarea
-                className="bg-black/20 thin-border p-2 text-white"
-                name={`${section.title}-${qIndex}`}
-                value={formData[`${section.title}-${qIndex}`] || ""}
-                onChange={(e) => handleTextAreaChange(`${section.title}-${qIndex}`, e.target.value)}
-              />
-            </div>
-          ))}
-        </div>
-      ))}
-
-      <div className="flex justify-between border-t border-white/30 mt-2 p-5">
-        <button
-          className="text-white px-4 py-2 rounded-md border disabled:opacity-50 hover:cursor-pointer disabled:hover:cursor-default text-xs sm:text-base hover-shine"
-          onClick={handlePrevious}
-          disabled={itemId === 1}
-        >
-          Previous
-        </button>
-        <button
-          type="submit"
-          className="bg-white text-black px-4 py-2 rounded-md disabled:opacity-50 hover:cursor-pointer disabled:hover:cursor-default text-xs sm:text-base hover-shine"
-        >
-          {itemId === frameworkItems.length ? "Generate Summary" : "Next"}
-        </button>
+    <>
+      <div className="w-full max-w-3xl self-center flex items-center justify-center gap-2 px-2 my-4">
+        {bars.map((step) => (
+          <Link
+            key={step}
+            href={`/framework?item=${step}`}
+            className="h-5 w-full flex items-center justify-center"
+          >
+            <div
+              className={`h-1 w-full rounded-full transition-all hover-shine ${
+                step <= itemId - 1 ? "bg-green" : "bg-green/50"
+              }`}
+            />
+          </Link>
+        ))}
       </div>
-    </form>
+
+      <form
+        className="flex flex-col thin-border w-full max-w-3xl self-center p-5 px-3 sm:px-5 text-white gap-4"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-xl font-bold">
+          {item.id}. {item.title}
+        </h2>
+        <p className="text-white text-sm">{item.description}</p>
+
+        {item.sections.map((section, index) => (
+          <div key={index} className="flex flex-col gap-2 thin-border p-5 px-3 sm:px-5">
+            <h3 className="text-green font-bold">{section.title}</h3>
+            {section.questions.map((question, qIndex) => (
+              <div key={qIndex} className="flex flex-col gap-1 py-2">
+                <label className="text-sm">{question}</label>
+                <textarea
+                  className="bg-black/20 thin-border p-2 text-white"
+                  name={`${section.title}-${qIndex}`}
+                  value={formData[`${section.title}-${qIndex}`] || ""}
+                  onChange={(e) =>
+                    handleTextAreaChange(`${section.title}-${qIndex}`, e.target.value)
+                  }
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+
+        <div className="flex justify-between border-t border-white/30 mt-2 p-5">
+          <button
+            className="text-white px-4 py-2 rounded-md border disabled:opacity-50 hover:cursor-pointer disabled:hover:cursor-default text-xs sm:text-base hover-shine"
+            onClick={handlePrevious}
+            disabled={itemId === 1}
+          >
+            Previous
+          </button>
+          <button
+            type="submit"
+            className="bg-white text-black px-4 py-2 rounded-md disabled:opacity-50 hover:cursor-pointer disabled:hover:cursor-default text-xs sm:text-base hover-shine"
+          >
+            {itemId === frameworkItems.length ? "Generate Summary" : "Next"}
+          </button>
+        </div>
+      </form>
+    </>
   )
 }
