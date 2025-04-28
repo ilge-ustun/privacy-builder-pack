dd24610f53f7e10197bcdf62f670a4116a26a02d
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-04-28 21:56:40 +0200

    fix footer css

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import InlineExternalLink from "@/components/InlineExternalLink"
-
 export default function Footer() {
   return (
     <footer className="w-full border-t border-white/20 text-white/60 py-6 px-6 md:px-24 text-center">
       Privacy Builder Pack © 2025 built by{" "}
-      <InlineExternalLink href="https://web3privacy.info/" text="Web3Privacy Now" /> | Resist
-      Surveillance
+      <a href="https://web3privacy.info/" target="_blank" rel="noopener noreferrer">
+        Web3Privacy Now
+      </a>{" "}
+      | Resist Surveillance
     </footer>
   )
 }
