2403ee5f7dd0f67a036bb50f38d78f540af639a7
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-04-29 17:54:56 +0200

    fix re-rendering

--- app/src/hooks/useGithubFiles.ts
+++ app/src/hooks/useGithubFiles.ts
@@ -1,4 +1,5 @@
 import { GitHubFile } from "@/types/gitHubFile"
+import { useMemo } from "react"
 
 interface UseGithubFilesOptions {
   repoOwner: string
@@ -13,33 +14,37 @@ export function useGithubFiles({
   folderPath,
   fileType = "md",
 }: UseGithubFilesOptions) {
-  const fetchFiles = async (): Promise<GitHubFile[]> => {
-    const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}`
+  const files = useMemo(() => {
+    const fetchFiles = async (): Promise<GitHubFile[]> => {
+      const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}`
 
-    const response = await fetch(apiUrl)
-    if (!response.ok) {
-      throw new Error(`Failed to fetch GitHub folder: ${response.status}`)
+      const response = await fetch(apiUrl)
+      if (!response.ok) {
+        throw new Error(`Failed to fetch GitHub folder: ${response.status}`)
+      }
+
+      const files: GitHubFile[] = await response.json()
+      const gitHubFiles = files.filter(
+        (file) => file.name.endsWith(`.${fileType}`) && file.name !== "contribute.md",
+      )
+
+      if (fileType === "png") {
+        return gitHubFiles as GitHubFile[]
+      }
+
+      const fetchedFiles = await Promise.all(
+        gitHubFiles.map(async (file) => {
+          const res = await fetch(file.download_url)
+          const text = await res.text()
+          return { name: file.name, content: text, download_url: file.download_url }
+        }),
+      )
+
+      return fetchedFiles as GitHubFile[]
     }
 
-    const files: GitHubFile[] = await response.json()
-    const gitHubFiles = files.filter(
-      (file) => file.name.endsWith(`.${fileType}`) && file.name !== "contribute.md",
-    )
+    return fetchFiles()
+  }, [repoOwner, repoName, folderPath, fileType])
 
-    if (fileType === "png") {
-      return gitHubFiles as GitHubFile[]
-    }
-
-    const fetchedFiles = await Promise.all(
-      gitHubFiles.map(async (file) => {
-        const res = await fetch(file.download_url)
-        const text = await res.text()
-        return { name: file.name, content: text, download_url: file.download_url }
-      }),
-    )
-
-    return fetchedFiles as GitHubFile[]
-  }
-
-  return { files: fetchFiles() }
+  return { files }
 }
